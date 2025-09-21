042efd4105d26d9e8a6dc304a64396452f6f3047
t <tattlelet@gmail.com>
2025-09-21 23:28:12 +0100

    Test undefined

--- src/lang/args/spec.zig
+++ src/lang/args/spec.zig
@@ -87,7 +87,6 @@ pub fn SpecResponse(comptime Spec: type) type {
             var self = try allocator.create(@This());
             self.arena = arena;
             self.codec = .{};
-            // TODO: test undefined
             self.options = .{};
             self.program = null;
             self.positionals = null;
@@ -275,7 +274,6 @@ test "empty args with default" {
     const r = try tstParse(allocator, "", struct {
         flag: bool = false,
     });
-    defer r.deinit();
     try std.testing.expectEqual(null, r.program);
     try std.testing.expectEqual(false, r.options.flag);
 }
@@ -287,7 +285,6 @@ test "program only" {
     const allocator = &arena.allocator();
 
     const r = try tstParse(allocator, "program", struct {});
-    defer r.deinit();
     try std.testing.expectEqualStrings("program", r.program.?);
 }
 
@@ -298,13 +295,9 @@ test "parse named" {
     const allocator = &arena.allocator();
 
     const r = try tstParse(allocator, "program --cool-flag", struct { @"cool-flag": bool = false });
-    defer r.deinit();
     const r2 = try tstParse(allocator, "program --cool-flag true", struct { @"cool-flag": bool = false });
-    defer r2.deinit();
     const r3 = try tstParse(allocator, "program --cool-flag false", struct { @"cool-flag": bool = false });
-    defer r3.deinit();
     const r4 = try tstParse(allocator, "program --cool-flag something else", struct { @"cool-flag": bool = false });
-    defer r4.deinit();
 
     try std.testing.expectEqual(true, r.options.@"cool-flag");
     try std.testing.expectEqual(true, r2.options.@"cool-flag");
@@ -321,11 +314,11 @@ test "parsed chained flags" {
     const allocator = &arena.allocator();
 
     const Spec = struct {
-        t1: ?bool = null,
-        t2: ?bool = null,
-        t3: ?bool = null,
-        t4: ?bool = null,
-        t5: ?bool = null,
+        t1: bool = undefined,
+        t2: bool = undefined,
+        t3: bool = undefined,
+        t4: bool = undefined,
+        t5: bool = undefined,
 
         pub const Short = .{
             .a = .t1,
@@ -336,12 +329,11 @@ test "parsed chained flags" {
         };
     };
     const r1 = try tstParse(allocator, "program -aaAbSsc=false", Spec);
-    defer r1.deinit();
-    try std.testing.expect(r1.options.t1.?);
-    try std.testing.expect(r1.options.t2.?);
-    try std.testing.expect(r1.options.t3.?);
-    try std.testing.expect(r1.options.t4.?);
-    try std.testing.expect(!r1.options.t5.?);
+    try std.testing.expect(r1.options.t1);
+    try std.testing.expect(r1.options.t2);
+    try std.testing.expect(r1.options.t3);
+    try std.testing.expect(r1.options.t4);
+    try std.testing.expect(!r1.options.t5);
 }
 
 test "parse short arg" {
@@ -360,13 +352,9 @@ test "parse short arg" {
         };
     };
     const r1 = try tstParse(allocator, "program -s --super-something true", Spec);
-    defer r1.deinit();
     const r2 = try tstParse(allocator, "program --something false -S", Spec);
-    defer r2.deinit();
     const r3 = try tstParse(allocator, "program -s false -S false", Spec);
-    defer r3.deinit();
     const r4 = try tstParse(allocator, "program -s true -S true", Spec);
-    defer r4.deinit();
 
     try std.testing.expectEqual(true, r1.options.something);
     try std.testing.expectEqual(true, r1.options.@"super-something");
@@ -385,25 +373,25 @@ test "parse different types" {
     const allocator = &arena.allocator();
 
     const Spec = struct {
-        o1: ?bool = null,
-        u1: ?u1 = null,
-        f1: ?f16 = null,
-        b: ?bool = null,
-        e: ?Animal = null,
-        s1: ?[]const u8 = null,
-        s2: ?[:0]const u8 = null,
-        au1: ?[]const u1 = null,
-        au32: ?[]const u32 = null,
-        ai32: ?[]const i32 = null,
-        af32: ?[]const f32 = null,
-        as1: ?[]const []const u8 = null,
-        as2: ?[]const [:0]const u8 = null,
-        aau32: ?[]const []const u32 = null,
-        aaf32: ?[]const []const f32 = null,
-        aai32: ?[]const []const i32 = null,
-        aab: ?[]const []const bool = null,
-        ae: ?[]const Animal = null,
-        o2: ?bool = null,
+        o1: bool = undefined,
+        u1: u1 = undefined,
+        f1: f16 = undefined,
+        b: bool = undefined,
+        e: Animal = undefined,
+        s1: []const u8 = undefined,
+        s2: [:0]const u8 = undefined,
+        au1: []const u1 = undefined,
+        au32: []const u32 = undefined,
+        ai32: []const i32 = undefined,
+        af32: []const f32 = undefined,
+        as1: []const []const u8 = undefined,
+        as2: []const [:0]const u8 = undefined,
+        aau32: []const []const u32 = undefined,
+        aaf32: []const []const f32 = undefined,
+        aai32: []const []const i32 = undefined,
+        aab: []const []const bool = undefined,
+        ae: []const Animal = undefined,
+        o2: bool = undefined,
 
         pub const Animal = enum { dog, cat };
     };
@@ -420,45 +408,44 @@ test "parse different types" {
         \\--s2 "Hello World"
         \\--au1 [1,0,1,0]
         \\--au32=[32,23,133,99,10]
-        \\--ai32 "[-1, -44, 22222   ,   -1]"
-        \\--af32="[    3.4   , 58,   3.1  ]"
+        \\--ai32 "[-1, -44, 22222, -1]"
+        \\--af32="[3.4, 58, 3.1]"
         \\--as1 "['Hello', ' World ', '!']"
-        \\--as2="[  'Im'  ,  'Losing it'  ]"
+        \\--as2="['Im', 'Losing it']"
         \\--aau32 "[[1,3], [3300, 222, 333, 33], [1]]"
-        \\--aaf32 "[  [  1.1,  3.2,-1] ,   [3.1  ,2,5], [ 1.2 ], [7.1]   ]"
-        \\--aai32 "[  [  -1] ,   [2, -2] ]"
+        \\--aaf32 "[[1.1, 3.2, -1], [3.1, 2,5], [1.2], [7.1]]"
+        \\--aai32 "[[-1], [2, -2]]"
         \\--aab="[[true, true, false], [false, true]]"
         \\--ae "[cat, cat]"
         \\--o2
     ,
         Spec,
     );
-    defer r1.deinit();
 
-    try std.testing.expect(r1.options.o1.?);
-    try std.testing.expectEqual(0, r1.options.u1.?);
-    try std.testing.expectEqual(1.1, r1.options.f1.?);
-    try std.testing.expect(!r1.options.b.?);
-    try std.testing.expectEqual(Spec.Animal.dog, r1.options.e.?);
-    try std.testing.expectEqualStrings("Hello", r1.options.s1.?);
-    try std.testing.expectEqualStrings("Hello World", r1.options.s2.?);
-    try std.testing.expectEqualDeep(&[_]u1{ 1, 0, 1, 0 }, r1.options.au1.?);
-    try std.testing.expectEqualDeep(&[_]u32{ 32, 23, 133, 99, 10 }, r1.options.au32.?);
-    try std.testing.expectEqualDeep(&[_]i32{ -1, -44, 22222, -1 }, r1.options.ai32.?);
+    try std.testing.expect(r1.options.o1);
+    try std.testing.expectEqual(0, r1.options.u1);
+    try std.testing.expectEqual(1.1, r1.options.f1);
+    try std.testing.expect(!r1.options.b);
+    try std.testing.expectEqual(Spec.Animal.dog, r1.options.e);
+    try std.testing.expectEqualStrings("Hello", r1.options.s1);
+    try std.testing.expectEqualStrings("Hello World", r1.options.s2);
+    try std.testing.expectEqualDeep(&[_]u1{ 1, 0, 1, 0 }, r1.options.au1);
+    try std.testing.expectEqualDeep(&[_]u32{ 32, 23, 133, 99, 10 }, r1.options.au32);
+    try std.testing.expectEqualDeep(&[_]i32{ -1, -44, 22222, -1 }, r1.options.ai32);
     const expectedAs1: []const []const u8 = &.{ "Hello", " World ", "!" };
-    try std.testing.expectEqualDeep(expectedAs1, r1.options.as1.?);
+    try std.testing.expectEqualDeep(expectedAs1, r1.options.as1);
     const expectedAs2: []const [:0]const u8 = &.{ "Im", "Losing it" };
-    try std.testing.expectEqualDeep(expectedAs2, r1.options.as2.?);
+    try std.testing.expectEqualDeep(expectedAs2, r1.options.as2);
     const expectedAau32: []const [:0]const u32 = &.{ &.{ 1, 3 }, &.{ 3300, 222, 333, 33 }, &.{1} };
-    try std.testing.expectEqualDeep(expectedAau32, r1.options.aau32.?);
+    try std.testing.expectEqualDeep(expectedAau32, r1.options.aau32);
     const expectedAaf32: []const []const f32 = &.{ &.{ 1.1, 3.2, -1 }, &.{ 3.1, 2, 5 }, &.{1.2}, &.{7.1} };
-    try std.testing.expectEqualDeep(expectedAaf32, r1.options.aaf32.?);
+    try std.testing.expectEqualDeep(expectedAaf32, r1.options.aaf32);
     const expectedAai32: []const []const i32 = &.{ &.{-1}, &.{ 2, -2 } };
-    try std.testing.expectEqualDeep(expectedAai32, r1.options.aai32.?);
+    try std.testing.expectEqualDeep(expectedAai32, r1.options.aai32);
     const expectAab: []const []const bool = &.{ &.{ true, true, false }, &.{ false, true } };
-    try std.testing.expectEqualDeep(expectAab, r1.options.aab.?);
-    try std.testing.expectEqualDeep(&[_]Spec.Animal{ .cat, .cat }, r1.options.ae.?);
-    try std.testing.expect(r1.options.o2.?);
+    try std.testing.expectEqualDeep(expectAab, r1.options.aab);
+    try std.testing.expectEqualDeep(&[_]Spec.Animal{ .cat, .cat }, r1.options.ae);
+    try std.testing.expect(r1.options.o2);
 }
 
 test "parse kvargs" {
@@ -478,7 +465,6 @@ test "parse kvargs" {
     };
 
     const r1 = try tstParse(allocator, "program -s=true --super-something=true", Spec);
-    defer r1.deinit();
 
     try std.testing.expectEqual(true, r1.options.something);
     try std.testing.expectEqual(true, r1.options.@"super-something");
@@ -492,17 +478,14 @@ test "parse positionals" {
     const allocator = &arena.allocator();
 
     const r1 = try tstParse(allocator, "program positional1 positional2 positional3", struct {});
-    defer r1.deinit();
     const expectedPositionals: []const []const u8 = &.{ "positional1", "positional2", "positional3" };
     try std.testing.expectEqualDeep(expectedPositionals, r1.positionals);
 
     const r2 = try tstParse(allocator, "program --test positional1 positional2 positional3", struct { @"test": bool = false });
-    defer r2.deinit();
     try std.testing.expect(r2.options.@"test");
     try std.testing.expectEqualDeep(expectedPositionals, r2.positionals);
 
     const r3 = try tstParse(allocator, "program -- --test positional1 positional2 positional3", struct { @"test": bool = false });
-    defer r3.deinit();
     try std.testing.expect(!r3.options.@"test");
     const expectSkip: []const []const u8 = &.{ "--test", "positional1", "positional2", "positional3" };
     try std.testing.expectEqualDeep(expectSkip, r3.positionals);
@@ -516,11 +499,7 @@ test "parse positionals" {
             pub const Verb = union(enum) { verb1: Verb1 };
         },
     );
-    defer r4.deinit();
-
-    // const expectSkip2: []const []const u8 = &.{ "--test", "positional1", "positional2", "positional3" };
     try std.testing.expect(r4.options.@"test");
-
     const expectL1Pos: []const []const u8 = &.{ "-", "pos1" };
     try std.testing.expectEqualDeep(expectL1Pos, r4.positionals.?);
     const expectL2Pos: []const []const u8 = &.{ "pos2", "pos3" };
@@ -537,11 +516,11 @@ test "parse verb" {
         verbose: ?bool = null,
 
         pub const Copy = struct {
-            src: ?[]const u8 = null,
+            src: []const u8 = undefined,
         };
 
         pub const Paste = struct {
-            target: ?[]const u8 = null,
+            target: []const u8 = undefined,
         };
 
         pub const Verb = union(enum) {
@@ -555,46 +534,42 @@ test "parse verb" {
     };
     try std.testing.expectError(GroupTracker(Spec).Error.MissingVerb, tstParse(allocator, "program", Spec));
     const r1 = try tstParse(allocator, "program copy --src file1", Spec);
-    defer r1.deinit();
     const r2 = try tstParse(allocator, "program paste --target file2", Spec);
-    defer r2.deinit();
     const r3 = try tstParse(allocator, "program --verbose false copy --src file3", Spec);
-    defer r3.deinit();
     const r4 = try tstParse(allocator, "program --verbose true paste --target file4", Spec);
-    defer r4.deinit();
     const r5 = try tstParse(allocator, "program --verbose true paste --target file4 positional1", Spec);
-    defer r5.deinit();
 
     try std.testing.expectEqual(Spec.Copy, @TypeOf(r1.verb.?.copy.options));
-    try std.testing.expectEqualStrings("file1", r1.verb.?.copy.options.src.?);
+    try std.testing.expectEqualStrings("file1", r1.verb.?.copy.options.src);
     try std.testing.expectEqual(null, r1.options.verbose);
     try std.testing.expectEqual(Spec.Paste, @TypeOf(r2.verb.?.paste.options));
-    try std.testing.expectEqualStrings("file2", r2.verb.?.paste.options.target.?);
+    try std.testing.expectEqualStrings("file2", r2.verb.?.paste.options.target);
     try std.testing.expectEqual(null, r2.options.verbose);
     try std.testing.expectEqual(Spec.Copy, @TypeOf(r3.verb.?.copy.options));
-    try std.testing.expectEqualStrings("file3", r3.verb.?.copy.options.src.?);
+    try std.testing.expectEqualStrings("file3", r3.verb.?.copy.options.src);
     try std.testing.expectEqual(false, r3.options.verbose.?);
     try std.testing.expectEqual(Spec.Paste, @TypeOf(r4.verb.?.paste.options));
-    try std.testing.expectEqualStrings("file4", r4.verb.?.paste.options.target.?);
+    try std.testing.expectEqualStrings("file4", r4.verb.?.paste.options.target);
     try std.testing.expectEqual(true, r4.options.verbose.?);
     try std.testing.expectEqual(Spec.Paste, @TypeOf(r5.verb.?.paste.options));
-    try std.testing.expectEqualStrings("file4", r5.verb.?.paste.options.target.?);
-    try std.testing.expectEqual(true, r5.options.verbose.?);
+    try std.testing.expectEqualStrings("file4", r5.verb.?.paste.options.target);
+    try std.testing.expectEqual(true, r5.options.verbose);
     const expected: []const []const u8 = &.{"positional1"};
     try std.testing.expectEqualDeep(expected, r5.verb.?.paste.positionals);
 }
 
+// TODO: move to example
 test "parse with custom codec" {
     const base = &std.testing.allocator;
     var arena = std.heap.ArenaAllocator.init(base.*);
     defer arena.deinit();
     const allocator = &arena.allocator();
     const Spec = struct {
-        i: ?i32 = null,
-        i2: ?i32 = null,
-        arf: ?i32 = null,
-        // This is handled by CodecIn (not default)
-        b: ?bool = null,
+        i: i32 = undefined,
+        i2: i32 = undefined,
+        arf: i32 = undefined,
+        // NOTE: This is handled by CodecIn (not default)
+        b: bool = undefined,
 
         pub const SpecFieldEnum = std.meta.FieldEnum(@This());
         pub const Spc = @This();
@@ -684,13 +659,13 @@ test "parse with custom codec" {
     ,
         Spec,
     );
-    defer r.deinit();
-    try std.testing.expectEqual(62, r.options.i.?);
-    try std.testing.expectEqual(31, r.options.i2.?);
-    try std.testing.expectEqual(15, r.options.arf.?);
-    try std.testing.expectEqual(true, r.options.b.?);
+    try std.testing.expectEqual(62, r.options.i);
+    try std.testing.expectEqual(31, r.options.i2);
+    try std.testing.expectEqual(15, r.options.arf);
+    try std.testing.expectEqual(true, r.options.b);
 }
 
+// TODO: move to example
 test "parse verb with custom codec" {
     const base = &std.testing.allocator;
     var arena = std.heap.ArenaAllocator.init(base.*);
@@ -701,11 +676,11 @@ test "parse verb with custom codec" {
         verbose: ?bool = null,
 
         pub const Copy = struct {
-            src: ?[]const u8 = null,
+            src: []const u8 = undefined,
         };
 
         pub const Paste = struct {
-            target: ?[]const u8 = null,
+            target: []const u8 = undefined,
 
             pub const SpecFieldEnum = std.meta.FieldEnum(@This());
             pub const Spc = @This();
@@ -780,11 +755,9 @@ test "parse verb with custom codec" {
         "program paste --target file1",
         Spec,
     );
-    defer r1.deinit();
-
     try std.testing.expectEqualStrings(
         "~/.config/file/file1.png",
-        r1.verb.?.paste.options.target.?,
+        r1.verb.?.paste.options.target,
     );
 
     try std.testing.expectError(
@@ -797,10 +770,9 @@ test "parse verb with custom codec" {
         "program copy --src file1",
         Spec,
     );
-    defer r2.deinit();
     try std.testing.expectEqualStrings(
         "file1",
-        r2.verb.?.copy.options.src.?,
+        r2.verb.?.copy.options.src,
     );
 }
 
@@ -811,35 +783,32 @@ test "validate require" {
     defer arena.deinit();
     const allocator = &arena.allocator();
 
-    const r = try tstParse(
-        allocator,
+    const Spec = struct {
+        i: i32 = undefined,
+        i2: i32 = undefined,
+        i3: ?i32 = null,
+        i4: ?i32 = null,
+        i5: ?i32 = null,
+        i6: ?i32 = null,
+
+        pub const GroupMatch: GroupMatchConfig(@This()) = .{
+            .mutuallyInclusive = &.{
+                &.{ .i3, .i4 },
+            },
+            .mutuallyExclusive = &.{
+                &.{ .i5, .i6 },
+            },
+            .required = &.{ .i, .i2 },
+        };
+    };
+    const r = try tstParse(allocator,
         \\program
         \\--i 1
         \\--i2 2
         \\--i3 3
         \\--i4 4
         \\--i6 5
-    ,
-        struct {
-            i: i32 = undefined,
-            i2: i32 = undefined,
-            i3: ?i32 = null,
-            i4: ?i32 = null,
-            i5: ?i32 = null,
-            i6: ?i32 = null,
-
-            pub const GroupMatch: GroupMatchConfig(@This()) = .{
-                .mutuallyInclusive = &.{
-                    &.{ .i3, .i4 },
-                },
-                .mutuallyExclusive = &.{
-                    &.{ .i5, .i6 },
-                },
-                .required = &.{ .i, .i2 },
-            };
-        },
-    );
-    defer r.deinit();
+    , Spec);
     try t.expectEqualStrings("program", r.program.?);
 }
 
@@ -859,7 +828,7 @@ test "parsed spec cleanup with verb" {
 
         pub const Copy = struct {
             // sentinels are cloned
-            src: ?[:0]const u8 = null,
+            src: [:0]const u8 = undefined,
         };
 
         pub const Verb = union(enum) {
